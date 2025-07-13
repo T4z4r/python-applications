8e9cb5f3cea0dc8dd42f25c1e49856a3a2451f78
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-07-13 13:56:51 +0300

    feat: largest of three numbers

---
+++ largest_of_three_numbers.py
@@ -0,0 +1,7 @@
+a=float(input("Enter the first number: "))
+b=float(input("Enter the second number: "))
+c=float(input("Enter the third number: "))
+
+max_num = max(a, b, c)
+
+print(f"The largest of the three numbers {a}, {b}, and {c} is: {max_num}")
