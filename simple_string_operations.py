70a92d68dcb1d6679410eabfeef3a0b3102e6b20
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-07-13 14:53:48 +0300

    feat: simple strings operations

---
+++ simple_string_operations.py
@@ -0,0 +1,7 @@
+string="Hello World!"
+print("Length of the string:", len(string))
+print("Uppercase:", string.upper())
+print("Lowercase:", string.lower())
+print("Titlecase:", string.title())
+print("Reversed string:", string[::-1])
+print("String contains 'World':", "World" in string)
