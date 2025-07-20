122a1b879802bb5a955b87e3a11d85bc4c1c3e9f
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-07-20 15:53:02 +0300

    feat: area and perimeter of a rectangle

---
+++ area_and_perimeter_of_rectangle.py
@@ -0,0 +1,5 @@
+length=float(input("Enter the length of the rectangle: "))
+width=float(input("Enter the width of the rectangle: "))
+area=length*width
+perimeter=2*(length+width)
+print(f"Area:{area},Perimeter:{perimeter}")
