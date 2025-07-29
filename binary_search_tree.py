5838f0826b73a790fca89e18d7d85b5190d9940b
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-07-29 22:32:50 +0300

    feat: Binary Tree search

---
+++ binary_search_tree.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.value = key
+        
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        if self.root is None:
+            self.root = Node(key)
+        else:
+            self._insert_rec(self.root, key)
+
+    def _insert_rec(self, node, key):
+        if key < node.value:
+            if node.left is None:
+                node.left = Node(key)
+            else:
+                self._insert_rec(node.left, key)
+        else:
+            if node.right is None:
+                node.right = Node(key)
+            else:
+                self._insert_rec(node.right, key)
+
+#Example usage
+tree=BinaryTree()
+tree.insert(10)
+tree.insert(5)
+tree.insert(15)
