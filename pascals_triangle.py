0bab05d6e75dab2f3a7fbdae079b98fa12916c67
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-07-29 22:20:27 +0300

    feat: pascals triangle

---
+++ pascals_triangle.py
@@ -0,0 +1,14 @@
+def generate_pascals_triangle(n):
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+    return triangle
+
+rows=5
+
+print("Pascal's Triangle:")
+for row in generate_pascals_triangle(rows):
+    print( row)
