29c2b3a0dea46c936e54bc3f4b640fb0c99a634e
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-07-29 23:19:56 +0300

    feat: calculate exponent value

---
+++ calculate_exponential_value.py
@@ -0,0 +1,5 @@
+base=float(input("Enter the base of the exponential function: "))
+exponent=float(input("Enter the exponent of the exponential function: "))
+result = base ** exponent
+
+print(f"The result of {base} raised to the power of {exponent} is: {result:.2f}")
